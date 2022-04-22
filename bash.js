7f09784d8bbd83436b9c8c627f32f68c0806a84f
msamori <mahmoud.samori@gmail.com>
2022-04-22 11:46:26 -0500

    finished pwd

--- bash.js
+++ bash.js
@@ -3,7 +3,16 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', (data) =>{
   const cmd = data.toString().trim();
 
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
+  if (cmd === 'pwd'){
 
+    // got process.cwd from stackoverflow:
+    // https://stackoverflow.com/questions/3151436/how-can-i-get-the-current-directory-name-in-javascript
+
+    process.stdout.write(process.cwd());
+    process.stdout.write('\nprompt > ');
+  }
+  else {
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+  }
 });
